3cb1dfa073bc8c8f5cb78e44a086dbb57e7ecb59
MagicWord-cmd <719338909@qq.com>
2023-05-29 18:21:33 +0800

    20230529新增9，10，11课内容

---
+++ vue-project/src/App-09-(array-change-detection)/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h3>数组变化侦听</h3>
+  <!-- 变更方法 -->
+  <!-- vue能够侦听响应式数组的变更方法，并在他们被调用时触发相关更新。这些变更的方法包括：
+    push()
+    pop()
+    shift()
+    unshift()
+    splice()
+    sort()
+    reverse() -->
+
+  <!-- 替换一个数组 -->
+  <!-- 变更方法是对调用它们的 原数组 进行变更。相对的也有一些不可变（immutable）方法。
+    如 filter(), concat(),  slice(),这些都不会更改原数组，而总是返回一个新数组。当
+    遇到的是非变更方法时，我们需要将旧的数组替换为新的 -->
+
+
+  <button @click="addListHandle">变更方法添加数据</button>
+  <button @click="addListHandle2">不可变方法添加数据</button>
+  <li v-for="(name,index) in names" :key="index">{{name}}</li>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        names: ['C++', 'Java', 'Python']
+      }
+    },
+
+    methods: {
+      addListHandle() {
+
+        //  变更方法会引起当前渲染的更新
+        this.names.push('C#')
+        console.log(this.names);
+      },
+      addListHandle2() {
+
+        //  不可变方法添加数据不会引起当前渲染的更新
+        this.names.concat(['C']);
+        console.log(this.names.concat(['C']));
+        //  要想让原数组发生变化并更新渲染需要对原数组重新赋值
+        this.names = this.names.concat(['C']);
+      }
+    }
+  }
+
+</script>
---
+++ vue-project/src/App-10-(calculation-properties)/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h3>计算属性</h3>
+  <!-- 模板（{{}}）中的表达式虽然很方便，但也只能用来做简单的操作。如果在模板中
+    写太多逻辑，会让模板变得臃肿，难以维护。因此我们推荐使用计算属性来
+    描述依赖响应式状态的复杂逻辑 -->
+
+    <!-- 计算属性缓存computed VS 方法methods -->
+    <!-- 你可能注意到我们在表达式中像这样用一个函数也会获得和计算属性相同的结果 -->
+    <!-- 重点区别在于：
+      计算属性会基于其响应式依赖被《缓存》 。一个计算属性仅会在其响应式依赖更新时
+      才重新计算，多次调用时可以重复使用已缓存的数据。
+      方法调用总是会在重渲染发生时再次执行，每次调用需要重新计算 -->
+
+  <h3>{{IT.name}}</h3>
+  <!-- 常规写法 -->
+  <p>{{IT.language.length > 0 ? 'yes' : 'no'}}</p>
+  <!-- 计算属性 -->
+  <p>{{returnIT}}</p>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+        IT: {
+          name: "MagicWord",
+          language: ['C', 'Java', 'Python']
+        }
+      }
+    },
+
+    //  计算属性
+    computed: {
+
+      returnIT() {
+        return this.IT.language.length > 0 ? 'YES' : 'NO'
+      }
+
+    }
+  }
+
+</script>
