95fac202c1f1fd6f2ede15594ffde82a76cafa07
MagicWord-cmd <719338909@qq.com>
2023-06-29 14:51:43 +0800

    1111

--- three-case-library/src/case-library/16-lines-dashed-animation/main.js
+++ three-case-library/src/case-library/16-lines-dashed-animation/main.js
@@ -22,11 +22,12 @@ function init() {
 	scene.background = new THREE.Color(0x111111);
 	scene.fog = new THREE.Fog(0x111111, 150, 200);
 
-	const subdivisions = 6;
-	const recursion = 1;
+	const subdivisions = 6;	//todo	影响曲线的分段
+	const recursion = 2;	//todo	重复循环（为1曲线全部渲染，<1不能全部渲染，>1会重复）
 	material = new THREE.LineDashedMaterial({ color: 0xffaa00, dashSize: 3, gapSize: 1, scale: 1 });
 
-	const points = GeometryUtils.hilbert3D(new THREE.Vector3(0, 0, 0), 25, recursion, 0, 1, 2, 3, 4, 5, 6, 7);
+	// const points = GeometryUtils.hilbert3D(new THREE.Vector3(0, 0, 0), 25, recursion, 0, 1, 2, 3, 4, 5, 6, 7);
+	const points = [new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 50, 0), new THREE.Vector3(50, 50, 0), new THREE.Vector3(50, 50, 50)]
 	const spline = new THREE.CatmullRomCurve3(points);
 
 	const samples = spline.getPoints(points.length * subdivisions);
@@ -59,14 +60,14 @@ function init() {
 	//todo 虚线滚动动画(虚线过长的话越接近末端，动画效果越不明显)
 	const scaleKF = new THREE.NumberKeyframeTrack(
 		'.material.scale',
-		[0, 1, 2, 3, 4],
-		[1,0.975,0.95,0.925,0.9]
+		[0, 1, 2, 3, 4, 5, 6, 7],
+		[1, 0.9, 0.8, 0.7, 0.6, 0.7, 0.8, 0.9]
 	);
-	const clip = new THREE.AnimationClip('Action', 4, [scaleKF]);
+	const clip = new THREE.AnimationClip('Action', 6, [scaleKF]);
 	mixer = new THREE.AnimationMixer(lineSegments);
 	clipAction = mixer.clipAction(clip);
 	clipAction.play();
-	//
+
 
 	window.addEventListener('resize', onWindowResize);
 
